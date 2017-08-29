f49fd1879985c570ac502eb16e2340dcf83cd816
DuckDivers <howarde@scubaboard.com>
2017-08-29 06:54:05 -0400

    UPD Headers

--- parallax-image.php
+++ parallax-image.php
@@ -2,8 +2,16 @@
 /*
   	Plugin Name: Parallax Image
   	Plugin URI: https://www.duckdiverllc.com/parallax-image-plugin/
-  	Version: 1.4
-  	Author: thehowarde
+  	Version: 1.4.1
+  	Contributors: thehowarde
+	Author: Howard Ehrenberg
+	Author URI: https://www.howardehrenberg.com
+	Donate link: https://www.duckdiverllc.com/parallax-image-plugin/
+	Tags: Parallax, Full Screen Parallax, Parallax Window, Parallax Image
+	Requires at least: 4.5
+	Tested up to: 4.8.1
+	Stable tag: 1.4.1
+	Requires PHP: 5.4
   	Description: A Simple plugin to employ the parallax.js script by pixelcog.  Use the shortcode [dd-parallax] to use.  See readme.txt for complete instructions. Version 1.3 adds ability to easily offset parallax content
 	License:  GNU General Public License v3
 	License URI: http://www.gnu.org/licenses/gpl-3.0.html
--- readme.txt
+++ readme.txt
@@ -1,10 +1,13 @@
 === Parallax Image ===
 Contributors: thehowarde
+Author: Howard Ehrenberg
+Author URI: https://www.howardehrenberg.com
 Donate link: https://www.duckdiverllc.com/parallax-image-plugin/
 Tags: Parallax, Full Screen Parallax, Parallax Window, Parallax Image
 Requires at least: 4.5
-Tested up to: 4.8
-Stable tag: 1.4
+Tested up to: 4.8.1
+Stable tag: 1.4.1
+Requires PHP: 5.4
 License: GPLv3 
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
